b44f65efbac3a020b40043e8b1fcee9b2f0cd307
LEA_HASELSTEINER <leahaselsteiner@hotmail.com>
2024-11-20 23:12:55 +0100

    button

--- src/components/climbing.jsx
+++ src/components/climbing.jsx
@@ -10,6 +10,7 @@ return(
     <p className="text">Would you like to {props.activityDescription}?</p> 
     <p><b>Good to know: {props.skills ? "This activity requires experience." : "This activity does not require experience."}</b></p>
     <p>Price: {props.priceTag}$</p>
+    <button>Let's go</button>
     </div>
     </>
 )
