3588298ba3714c1042516311a04e7a4d0c91e780
LEA_HASELSTEINER <leahaselsteiner@hotmail.com>
2024-11-25 10:33:30 +0100

    navbar works

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@
 import { BrowserRouter , Routes , Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
+import Navbar from "./Navbar.jsx";
 
 function App() {
   return (
     <>
     <BrowserRouter>
+    <Navbar></Navbar>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
