70b23caadc791bbeac3127cf6894925a4f79d4ab
LEA_HASELSTEINER <leahaselsteiner@hotmail.com>
2024-11-25 12:13:24 +0100

    navbar css

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,12 @@
 import {Link} from "react-router-dom";
 
 function Navbar(){
-    return <div>
+    return <div className="nav-container">
         <nav>
             <ul>
-                <li><Link to="/" >Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/" className="nav-link">Home</Link></li>
+                <li><Link to="/about" className="nav-link">About</Link></li>
+                <li><Link to="/contact" className="nav-link">Contact</Link></li>
             </ul>
         </nav>
     </div>
