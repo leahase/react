ec374a1e78eeb1e9642d519747e1116d0ec3de50
LEA_HASELSTEINER <leahaselsteiner@hotmail.com>
2024-11-27 13:30:27 +0100

    color-palette and set color

--- src/pages/Color.jsx
+++ src/pages/Color.jsx
@@ -1,6 +1,9 @@
+import {ColorPicker, useColor } from "react-color-palette";
+import "react-color-palette/css";
+
 function Color() {
-    
-    return <div> Color-picker
+    const [color, setColor] = useColor("#165321er");
+    return <div> <ColorPicker color={color} onChange={setColor} />;
     </div>
 }
 export default Color;
