d5de8c36401672a0c0c5350f7b155c69bf483ff7
LEA_HASELSTEINER <leahaselsteiner@hotmail.com>
2024-12-09 23:10:49 +0100

    inout, task

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -5,14 +5,27 @@ function Todo() {
     const [tasks, setTasks] = useState([]);
 
   const addTask =() => { //fuction to add task
-    
-  }
+    if (task.trim() !== '') {
+        setTasks([...tasks, task]);
+        setTask('');
+    }
+  };
 
   const removeTask =() => { //fuction to remove task
 
   }
     return <div> <h1>To do list</h1>
-    <p>addd your items</p>
+    <p>add your items</p>
+    <input
+        type="text"
+        value={task}
+        onChange={() setTask()
+        placeholder="Enter your to do"
+    />
+    <button onClick={addTask}></button>
+    <ul>
+       
+    </ul>
     </div>
 }
 export default Todo;
