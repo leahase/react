639ff54221e944e8b86f2db852dca432db0c0d7e
LEA_HASELSTEINER <leahaselsteiner@hotmail.com>
2024-12-09 23:02:12 +0100

    functions

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -3,7 +3,14 @@ import React, { useState } from 'react';
 function Todo() {
     const [task, setTask] = useState('');
     const [tasks, setTasks] = useState([]);
-  
+
+  const addTask =() => { //fuction to add task
+
+  }
+
+  const removeTask =() => { //fuction to remove task
+
+  }
     return <div> <h1>To do list</h1>
     <p>addd your items</p>
     </div>
