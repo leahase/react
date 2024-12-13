59f9355912ddb67cf9626317867a93b43e7b2849
LEA_HASELSTEINER <leahaselsteiner@hotmail.com>
2024-12-13 22:04:06 +0100

    trivia api

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -2,25 +2,23 @@ import "../index.css";
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
-    const Search = () => {
-      const [data, setData] = useState([]);
+   
+      function Search (){
+        const [trivia, setTrivia] = useState("");
+    
+
+      const generateTrivia = () =>{
+        axios.get("https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean").then(response => {
+            setTrivia(response.data.results);
+        });
       };
-
-      useEffect(() => {
-        axios.get()
-        .then(response => {
-            setPosts(response.data);
-        })
-        .catch(error =>
-        console.error(error);
-    )
-      })
-
    
       return (
         <div>
-            <h2>Search: {query}</h2>
+            <button onClick={generateTrivia}>Search for Trivia questions</button>
+            <h2>Trivia</h2>
+
         </div>
-      )
+      );
+    }
     export default Search;
-    
