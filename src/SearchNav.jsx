f3d4d22ae727352b03905ad2cc699fcd5ce392ef
LEA_HASELSTEINER <leahaselsteiner@hotmail.com>
2024-12-15 09:14:47 +0100

    search nav

---
+++ src/SearchNav.jsx
@@ -0,0 +1,18 @@
+import {Link} from "react-router-dom";
+
+function Navbar(){
+    return <div className="search-nav-container">
+        <nav>
+            <ul>
+                <li><Link to="/" className="search-nav-link">Search</Link></li>
+                <li><Link to="/about" className="search-nav-link">Images</Link></li>
+                <li><Link to="/contact" className="search-nav-link">Videos</Link></li>
+                <li><Link to="/color" className="search-nav-link">Maps</Link></li>
+                <li><Link to="/todo" className="search-nav-link">News</Link></li>
+                <li><Link to="/search" className="search-nav-link">Products</Link></li>
+                <button class="drop-sign">Sign in</button>
+            </ul>
+        </nav>
+    </div>
+}
+export default Navbar;
